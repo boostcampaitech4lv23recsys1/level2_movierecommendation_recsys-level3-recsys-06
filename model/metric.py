e5bdd005624afa4bd81e3277ecd0cf45a1f8ebc5
hwanseung2 <wazs555@hufs.ac.kr>
2022-12-30 09:59:19 +0000

    feat: Add recall@k metric code (#6)

--- model/metric.py
+++ model/metric.py
@@ -1,3 +1,5 @@
+import numpy as np
+from collections import defaultdict
 import torch
 
 
@@ -18,3 +20,22 @@ def top_k_acc(output, target, k=3):
         for i in range(k):
             correct += torch.sum(pred[:, i] == target).item()
     return correct / len(target)
+
+
+def recallk(output, target, k = 10):
+    avg_recallk = np.array([])
+    eps = 1e-7
+    users = list(output['user'].unique())
+    output_dict = defaultdict(list)
+    target_dict = defaultdict(list)
+
+    for row in output.iterrows():
+        output_dict[row[1]['user']].append(row[1]['item'])
+    for row in target.iterrows():
+        target_dict[row[1]['user']].append(row[1]['item'])
+    
+    for user in users:
+        inter = len(set(output_dict[user][:k]) & set(target_dict[user]))
+        div = len(target_dict[user]) + eps
+        avg_recallk = np.append(avg_recallk, (inter / div))
+    return avg_recallk.mean()
