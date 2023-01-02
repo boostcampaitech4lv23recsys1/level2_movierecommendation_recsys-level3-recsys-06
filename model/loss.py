bc4f6536cbc1297ebe488823fbb5458eba13943e
huijaeshin <suhpysiss@gmail.com>
2023-01-02 06:10:02 +0000

    feat: AutoEncoders loss and metric

--- model/loss.py
+++ model/loss.py
@@ -1,5 +1,16 @@
 import torch.nn.functional as F
+import torch
 
 
 def nll_loss(output, target):
     return F.nll_loss(output, target)
+
+def loss_function_dae(recon_x, x):
+    BCE = -torch.mean(torch.sum(F.log_softmax(recon_x, 1) * x, -1))
+    return BCE
+
+def loss_function_vae(recon_x, x, mu, logvar, anneal=1.0):
+    BCE = -torch.mean(torch.sum(F.log_softmax(recon_x, 1) * x, -1))
+    KLD = -0.5 * torch.mean(torch.sum(1 + logvar - mu.pow(2) - logvar.exp(), dim=1))
+
+    return BCE + anneal * KLD
